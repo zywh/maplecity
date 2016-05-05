1d8106f4bfe146f461a50bfb1abe49e15f30f604
zhengying@yahoo.com <zhengying@yahoo.com>
2016-05-05 12:10:05 +0800

    autobackup 201605051210

--- themes/house/views/site/index.php
+++ themes/house/views/site/index.php
@@ -14,10 +14,8 @@
 
 .swiper-container { background-color:#888787;}
 .swiper-slide img { 
-	width: 330px; height: 330px;  
-	padding:4px;
-    border:1px solid #888787;
-    background-color:#fff;}
+	width: 100%; height: 430px;  
+	}
 .swiper-pagination-bullet { opacity: 1; background: #fff; }
 .swiper-pagination-bullet-active { opacity: 1; background: #ff4103; }
 </style>
@@ -68,9 +66,10 @@
 				
 				 <?php foreach($subject_list as $project){ ?>
 					<div class="swiper-slide" >
-					<img style="width: 100%;height: 250px;"	src="<?php 
+					<a data-ajax="false" href="<?php echo Yii::app()->createUrl('projects/more',array('id'=>$project->id)); ?>">
+					<img src="<?php 
 					echo Yii::app()->request->baseUrl;?>/<?php echo $project->room_type_image; ?>">
-					
+					</a>
 				
 					</div>
 				<?php }?> 
@@ -95,36 +94,7 @@
 
 	
 	</div>
-<script>
-	var swiper = new Swiper(".swiper-container", {
-			pagination: '.swiper-pagination',
-			 nextButton: '.swiper-button-next',
-			prevButton: '.swiper-button-prev',
-			slidesPerView: 3,
-			effect: 'coverflow',
-			centeredSlides: true,
-			//slidesPerView: 'auto',
-			coverflow: {
-				rotate: 50,
-				stretch: 0,
-				depth: 100,
-				modifier: 1,
-				//slideShadows : true
-			},
-			preloadImages: false,
-			lazyLoading: true,
-			paginationClickable: true,
-			spaceBetween: 22,
-			loop: true,
-			autoplay: 5000,
-			speed: 2000,
-			//scrollbar: '.swiper-scrollbar',
-			//scrollbarHide: true,
-			//grabCursor: true,
-			autoplayDisableOnInteraction: true
 
-	});
-</script>
 </div>
 <script type="text/javascript">
     //豪宅列表
@@ -144,7 +114,7 @@
 </script>
 
 <!-- banner结束 -->
-<!-- banner结束 -->
+
 
 <div class="nycont_mls">
 <div class="nycont_sgkjj"><p id="socialicons3">
@@ -277,115 +247,6 @@ foreach($resultshazai as $househaizai){
     <div class="cl"></div>
 </div>
 <!-- 热点推荐结束 -->
-
-
-<!-- 学区专栏开始 -->
-<div class="lm_three">
-    <div class="lm_threeup"><a><img src="/static/images/xuequ.jpg" width="980" height="39" /></a></div>
-<div>
-<!--开始 -->
-
-<style type="text/css">
-
-.blk_18 { overflow:hidden; zoom:1; font-size:9pt;width:984px; margin-top:8px; }
-.blk_18 .pcont { width:890px; float:left; overflow:hidden; padding-left:5px; }
-.blk_18 .ScrCont { width:32766px; zoom:1; margin-left:-5px; }
-.blk_18 #List1_1, .blk_18 #List2_1 { float:left; }
-.blk_18 .LeftBotton, .blk_18 .RightBotton { width:38px; height:65px; float:left; margin-top:150px; }
-.blk_18 .pl { width:228px;float:left; float:left; text-align:center; line-height:24px; }
-.blk_18 a{ text-decoration:none;}
-.blk_18 a.pl:hover { color:#5dacec; background:#fff; }
-.ScrCont .biaotiya { font-size:14px; font-weight:bold; color:#FF0000; width:211px; margin:0 auto;}
-.ScrCont .address{ text-align:left; width:211px; margin:0 auto; color:#5e5e5e} 
-.ScrCont .price{ text-align:left; width:211px; margin:0 auto;color:#5e5e5e} 
-.ScrCont .xiangxi{text-align:left; width:211px; margin:0 auto;color:#5e5e5e}
-
-</style>
-<script type="text/javascript">
-
-var Speed_1 = 260; //速度(毫秒)
-var Space_1 = 20; //每次移动(px)
-var PageWidth_1 = 107 * 6; //翻页宽度
-var interval_1 = 12000; //翻页间隔时间
-var fill_1 = 0; //整体移位
-var MoveLock_1 = false;
-var MoveTimeObj_1;
-var MoveWay_1="right";
-var Comp_1 = 0;
-var AutoPlayObj_1=null;
-function GetObj(objName){if(document.getElementById){return eval('document.getElementById("'+objName+'")')}else{return eval('document.all.'+objName)}}
-function AutoPlay_1(){clearInterval(AutoPlayObj_1);AutoPlayObj_1=setInterval('ISL_GoDown_1();ISL_StopDown_1();',interval_1)}
-function ISL_GoUp_1(){if(MoveLock_1)return;clearInterval(AutoPlayObj_1);MoveLock_1=true;MoveWay_1="left";MoveTimeObj_1=setInterval('ISL_ScrUp_1();',Speed_1);}
-function ISL_StopUp_1(){if(MoveWay_1 == "right"){return};clearInterval(MoveTimeObj_1);if((GetObj('ISL_Cont_1').scrollLeft-fill_1)%PageWidth_1!=0){Comp_1=fill_1-(GetObj('ISL_Cont_1').scrollLeft%PageWidth_1);CompScr_1()}else{MoveLock_1=false}
-AutoPlay_1()}
-function ISL_ScrUp_1(){if(GetObj('ISL_Cont_1').scrollLeft<=0){GetObj('ISL_Cont_1').scrollLeft=GetObj('ISL_Cont_1').scrollLeft+GetObj('List1_1').offsetWidth}
-GetObj('ISL_Cont_1').scrollLeft-=Space_1}
-function ISL_GoDown_1(){clearInterval(MoveTimeObj_1);if(MoveLock_1)return;clearInterval(AutoPlayObj_1);MoveLock_1=true;MoveWay_1="right";ISL_ScrDown_1();MoveTimeObj_1=setInterval('ISL_ScrDown_1()',Speed_1)}
-function ISL_StopDown_1(){if(MoveWay_1 == "left"){return};clearInterval(MoveTimeObj_1);if(GetObj('ISL_Cont_1').scrollLeft%PageWidth_1-(fill_1>=0?fill_1:fill_1+1)!=0){Comp_1=PageWidth_1-GetObj('ISL_Cont_1').scrollLeft%PageWidth_1+fill_1;CompScr_1()}else{MoveLock_1=false}
-AutoPlay_1()}
-function ISL_ScrDown_1(){if(GetObj('ISL_Cont_1').scrollLeft>=GetObj('List1_1').scrollWidth){GetObj('ISL_Cont_1').scrollLeft=GetObj('ISL_Cont_1').scrollLeft-GetObj('List1_1').scrollWidth}
-GetObj('ISL_Cont_1').scrollLeft+=Space_1}
-function CompScr_1(){if(Comp_1==0){MoveLock_1=false;return}
-var num,TempSpeed=Speed_1,TempSpace=Space_1;if(Math.abs(Comp_1)<PageWidth_1/2){TempSpace=Math.round(Math.abs(Comp_1/Space_1));if(TempSpace<1){TempSpace=1}}
-if(Comp_1<0){if(Comp_1<-TempSpace){Comp_1+=TempSpace;num=TempSpace}else{num=-Comp_1;Comp_1=0}
-GetObj('ISL_Cont_1').scrollLeft-=num;setTimeout('CompScr_1()',TempSpeed)}else{if(Comp_1>TempSpace){Comp_1-=TempSpace;num=TempSpace}else{num=Comp_1;Comp_1=0}
-GetObj('ISL_Cont_1').scrollLeft+=num;setTimeout('CompScr_1()',TempSpeed)}}
-function picrun_ini(){
-GetObj("List2_1").innerHTML=GetObj("List1_1").innerHTML;
-GetObj('ISL_Cont_1').scrollLeft=fill_1>=0?fill_1:GetObj('List1_1').scrollWidth-Math.abs(fill_1);
-GetObj("ISL_Cont_1").onmouseover=function(){clearInterval(AutoPlayObj_1)}
-GetObj("ISL_Cont_1").onmouseout=function(){AutoPlay_1()}
-AutoPlay_1();
-}
-</script>
-
-<div class="blk_18"> <a class="LeftBotton" onmousedown="ISL_GoUp_1()" onmouseup="ISL_StopUp_1()" onmouseout="ISL_StopUp_1()" href="javascript:void(0);" target="_self"><img src="/themes/house/images/zuobianya.jpg" border="0" /></a>
-  <div class="pcont" id="ISL_Cont_1">
-    <div class="ScrCont">
-      <div id="List1_1">
-        <!-- piclist begin -->
-        
-        
-<?php 
-$sqlxuequ = "select * from h_house where lp_dol<=2000000 and lp_dol >500000 limit 0,9 ";
-$resultxuequ = $db->createCommand($sqlxuequ)->query();
-foreach($resultxuequ as $housexuequ){
-?>
-        <a class="pl" href="<?php echo Yii::app()->createUrl('house/view',array('id'=>$housexuequ["id"])); ?>" target="_blank" >
-        <?php 
-				 
-					$county = $housexuequ["county"];
-					$ml_num = $housexuequ["ml_num"];
-					$picfile = get_firstimage($county,$ml_num);
-			?><img src="<?php echo Yii::app()->request->baseUrl;?>/<?php echo $picfile; ?>" width="260" height="250"/>
-		<div class="biaotiya"><?php echo $housexuequ["addr"]; ?></div>
-        <div class="address"><strong>城市：</strong><?php echo $housexuequ["municipality"]; ?></div>
-        <div class="price"><strong>价格：</strong><?php echo $housexuequ["lp_dol"]/10000; ?>万加币</div>
-        <div  class="xiangxi"><img src="/themes/house/images/xiangxi.jpg" /></div>
-        </a> 
-<?php } ?>
- 
-        <!-- piclist end -->
-      </div>
-      <div id="List2_1"></div>
-    </div>
-  </div>
-  <a class="RightBotton" onmousedown="ISL_GoDown_1()" onmouseup="ISL_StopDown_1()" onmouseout="ISL_StopDown_1()" href="javascript:void(0);" target="_self"><img src="/themes/house/images/youbianya.jpg" border="0" /></a> 
-  <div style="clear:both"></div>
-  </div>
-<div class="c"></div>
-<script type="text/javascript">
-        <!--
-        picrun_ini()
-        //-->
-        </script>
-
-
-<!--结束 -->
-    </div>
-</div>
-<div style="height:20px; clear:both"></div>
-<!-- 学区专栏结束 -->
 
 
 <!-- 加国资讯开始 -->
@@ -900,6 +761,27 @@ foreach($resultxuequ as $housexuequ){
     });
 
     $(document).ready(function(){
+		
+		//Swiper Start
+		var swiper = new Swiper(".swiper-container", {
+			pagination: '.swiper-pagination',
+			nextButton: '.swiper-button-next',
+			prevButton: '.swiper-button-prev',
+			effect: 'fade',
+			//slidesPerView: 1,
+			preloadImages: false,
+			lazyLoading: true,
+			paginationClickable: true,
+			//spaceBetween: 22,
+			loop: true,
+			autoplay: 5000,
+			speed: 2000,
+			autoplayDisableOnInteraction: false
+		
+
+		});
+		//Start End
+		
         //热点推荐
         $(".lm_jddown").mouseover(function(){
             $(this).find(".lm_jd_tcbox").addClass("lm_jd_tcbox_on")
